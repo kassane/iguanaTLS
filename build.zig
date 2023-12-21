3e8b6a1c6aec8570f904b3bb25c2c7d4dead78bd
Jeremy Daffon <jdaffon123@gmail.com>
2023-12-20 21:22:33 -0800

    Test for zon

--- build.zig
+++ build.zig
@@ -2,19 +2,13 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const optimize = b.standardOptimizeOption(.{});
-    const lib = b.addStaticLibrary(.{
-        .name = "iguanaTLS",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .optimize = optimize,
-        .target = .{},
+    _ = b.addModule("iguanaTLS", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
-    b.installArtifact(lib);
-
     var main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .optimize = optimize,
     });
-
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 }
